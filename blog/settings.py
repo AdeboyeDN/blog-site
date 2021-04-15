33ec706b2ca05fcbf3e6f882933ade1db8fa904c
daniel <adeboyed93@gmail.com>
2021-04-15 21:55:09 +0100

    'new'

--- blog/settings.py
+++ blog/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = '7$3y=d3_qf*rc^a&i_v)6)y%+f586jom82x6#l!j=(+qr^!ndg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = ['https://developerola-blog.herokuapp.com/', '127.0.0.1' ]
+#https://developerola-blog.herokuapp.com/
+ALLOWED_HOSTS = ['developerola-blog.herokuapp.com/' ]
 
 
 # Application definition
