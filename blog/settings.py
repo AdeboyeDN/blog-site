02b7ba20441acdd3bfe602798802616d181abf27
daniel <adeboyed93@gmail.com>
2021-04-16 06:57:24 +0100

    'new'

--- blog/settings.py
+++ blog/settings.py
@@ -60,7 +60,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.runserver_nostatic',
+
 ]
 
 ROOT_URLCONF = 'blog.urls'
