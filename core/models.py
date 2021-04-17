f091b2fc567ed1b63d6eb078ec3af5c4cf04c418
daniel <adeboyed93@gmail.com>
2021-04-17 08:07:26 +0100

    ''

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class blog(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL,blank=True, null=True)
     title = models.CharField(max_length=500)
-    image = models.ImageField(upload_to='images/',blank=True, null=True )
+    image = models.ImageField(blank=True, null=True )
     description = models.TextField(max_length=700)
     date = models.DateTimeField(auto_now_add=True, blank=True)
     slug = models.SlugField(blank=True, null=False)
